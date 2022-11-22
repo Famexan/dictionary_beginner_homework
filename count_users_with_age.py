be4c5d5250ae4858aa917632d4ee1bd2371c4526
fame <famephp@gmail.com>
2022-11-23 00:21:08 +0500

    02

--- count_users_with_age.py
+++ count_users_with_age.py
@@ -8,4 +8,18 @@ def count_users_with_age(data:list, age:int) -> int:
     Returns:
         int: The number of users with the given age
     """
-    pass
+    count = 0
+    for i in data:
+        for k,j in i.items():
+            if k == 'age' and j == x:
+                count += 1 
+    return count
+data = [
+    {'name ': 'John', 'job': 'Developer', 'age': 50},
+    {'name ': 'Mary', 'job': 'Developer', 'age': 50},
+    {'name ': 'Bary', 'job': 'Student', 'age': 50},
+    {'name ': 'Golib', 'job': 'Snayper', 'age': 60},
+]
+x = 60
+
+print(count_users_with_age(data,x))
