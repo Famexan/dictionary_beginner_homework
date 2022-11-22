19d532ae6faf3dbaf77e1c1abb1ef34ed6dcf309
fame <famephp@gmail.com>
2022-11-22 23:49:47 +0500

    01

--- count_jobs.py
+++ count_jobs.py
@@ -1,11 +1,25 @@
 def count_jobs(data:list, job:str) -> int:
     """
     Return the number of users with a given job
-
     Args:
         data (dict): A dictionary of values
         job (str): The job to search for
     Returns:
         int: The number of users with the given job
     """
-    pass
+    count = 0
+    for i in data:
+        for j in i.values():2
+        count += 1
+            
+    return count
+
+data = [
+    {'name ': 'John', 'job': 'Developer'},
+    {'name ': 'Mary', 'job': 'Developer'},
+    {'name ': 'Bary', 'job': 'Student'},
+    {'name ': 'Golib', 'job': 'Snayper'},
+]
+x = 'Developer'
+
+print(count_jobs(data,x))
