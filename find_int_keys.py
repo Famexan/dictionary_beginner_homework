efd2df18837f2639e22e50caf4bffe67c981fca4
fame <famephp@gmail.com>
2022-11-23 01:21:43 +0500

    03

--- find_int_keys.py
+++ find_int_keys.py
@@ -6,4 +6,19 @@ def find_int_keys(data: dict) -> list:
     Returns:
         list: A list of all keys in the dictionary that are integers.
     """
-    pass
+    count = []
+    for i in data:
+        for j in i:
+            if type(j) == int:
+                count.append(j)
+                
+    return count      
+    
+data = [
+    {'name ': 'John', 'job': 'Developer', 50: 'age'},
+    {'name ': 'Mary', 'job': 'Developer', 5.0: 'age'},
+    {'name ': 'Bary', 'job': 'Student', 50: 'age'},
+    {'name ': 'Golib', 'job': 'Snayper', 50: 'age'},
+]
+
+print(find_int_keys(data))
