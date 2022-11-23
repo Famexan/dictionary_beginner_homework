43b1e46e2c33e5ebc06d21c1fde65158af99c739
fame <famephp@gmail.com>
2022-11-23 19:02:41 +0500

    05

--- find_max_key.py
+++ find_max_key.py
@@ -6,4 +6,18 @@ def find_max_key(data: dict) -> int:
     Returns:
         int: The maximum key in the dictionary.
     """
-    pass
+    count = 0
+    for i in data:
+        for j in i.keys():
+            if type(j) == int and j > count:
+                count = j
+
+    return count
+    
+data = [
+    {'name ': 'John', 'job': 'Developer', 50: 'age'},
+    {'name ': 'Mary', 'job': 'Developer', 5.0: 'age'},
+    {'name ': 'Bary', 'job': 'Student', 60: 'age'},
+    {'name ': 'Golib', 'job': 'Snayper', 50: 'age'},
+]
+print(find_max_key(data))
